1e905e289adda8a5cb9e708faadda86308d912fd
ted1030 <37107237+ted1030@users.noreply.github.com>
2018-05-17 20:04:59 +0800

    修改使用道具特效

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var tool = [false,false,false,false,false];
 var working = false;
-var drag = false;
+var clicked = false;
 var obj = null;
 
 function slide(i){
@@ -23,17 +23,19 @@ function hide(i){
 }
 
 function make(e,dom){
-    drag = true;
+    event.stopPropagation();    
     obj = dom.clone();
+    obj.css("position","absolute");
     $("#main").append(obj);
-    obj.show();
-    obj.offset({
-		top: e.pageY - 40,
-		left: e.pageX - 40
-	});
+    obj.hide();
+    clicked = true;
 }
 
 $(document).ready(function(){
+    document.body.addEventListener('touchmove', function(event) { 
+        event.preventDefault(); 
+    }, false);
+
     $("#store").click(function(){
         $("#shop").css("box-shadow","5px 5px 10px 0.3px rgba(20%,20%,40%,0.3)");	
         $("#shop").animate({"width":"100vw"},500);
@@ -49,17 +51,27 @@ $(document).ready(function(){
     hide(4);
     $("#tool1").click(function(e){make(e,$("#watercan"));});
     $("[class^=tool]").click(function(e){make(e,$(this));});
-    $("#main").touchmove(function (e){
-        if (drag)
+    $("#main").click(function (e){
+        if (clicked)
         {
             obj.offset({
             top: e.pageY - 40,
             left: e.pageX - 40
-            });			
-        }
+            });
+            obj.show();
+            obj.css("transform");
+            if (obj.attr("src") == "ICON/watering-can (1).png")
+                obj.css("transform","rotate(-45deg)");
+            else if (obj.attr("class") == "tool2b" || obj.attr("class") == "tool4b")
+                obj.animate({"top": e.pageY + 10 +"px","left": e.pageX - 100 +"px"},600);
+            else if (obj.attr("class") == "tool3b")
+                obj.effect("bounce");
+            else
+                obj;
+            setTimeout(function(){
+                clicked = false;
+		        obj.remove();
+            },600);
+        }        
     });	
-    $("#main").mouseup(function(){
-        drag = false;
-		obj.remove();
-	});	
 });
