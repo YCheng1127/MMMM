20b0047536fc041455a969395e987ca7153cdbdd
YCheng <mgriffiba1127@gmail.com>
2018-06-27 10:42:47 +0800

    add the friend tree page

--- friendPage.js
+++ friendPage.js
@@ -15,8 +15,9 @@ function make_friends_face(){
   }
 }
 
-function make_tree_page(){
+function make_tree_page(data){
   var box=document.getElementById("friend");
+  var GV=JSON.parse(data);
   //底圖
   var Page=document.createElement("div");
   Page.style.backgroundImage="url(ICON2/種樹頁底圖.png)";
@@ -39,4 +40,43 @@ function make_tree_page(){
   Page_tree.setAttribute("id","friend_tree")
   Page_tree.style.zIndex="4";
   Page.appendChild(Page_tree);
+  //數值
+  var game_value=document.createElement("div");
+  game_value.setAttribute("id","friend_value");
+  game_value.style.position="absolute";
+  game_value.style.top="20%";
+  game_value.style.width="35%";
+  game_value.style.backgroundColor="#49e54942";
+  game_value.style.fontSize="5vw";
+  game_value.style.fontStyle="monospace"
+  game_value.style.zIndex="5";
+  game_value.style.left="3vw"
+  if(GV[0]!=null)
+    game_value.innerHTML="<p>   "+GV[0].name+" 's Tree</p> <p>   Year: "+GV[0].year+"</p> <p>   TurtleScore:　" +GV[0].TurtleScore+"</p> <p>   CastleScore: "+GV[0].CastleScore;
+  else
+    game_value.innerHTML="The Player doesn't exist";
+  Page.appendChild(game_value);
+  var killpage=document.createElement("div");
+  killpage.setAttribute("id","killpage")
+  Page.appendChild(killpage);
+  killpage.position="absolute";
+  killpage.style.width="7%";
+  right="3%";
+  bottom="3%";
+  killpage.innerHTML='<img id="killpagebutton" src="./png/game_back.png" onclick="killpage()">';
+}
+
+clickphoto=function(num){
+  if(myfriends[num]!=undefined){
+    getfrienddata(myfriends[num].id,function(data){
+       console.log(data);
+        make_tree_page(data);
+
+
+    })
+  }
+}
+
+killpage=function(){
+  document.getElementById("friend_tree_page").remove();
 }
